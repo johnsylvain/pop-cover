fabd7a3d10e8f096bb32319586a71d7d3ca51f96
John Sylvain <john.n.sylvain@gmail.com>
2019-06-10 07:54:14 -0500

    FEAT: set up functions

---
+++ functions/login.js
@@ -0,0 +1,10 @@
+exports.handler = function(event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      Location: 'https://accounts.spotify.com/authorize',
+      'Cache-Control': 'no-cache'
+    },
+    body: ''
+  });
+};
